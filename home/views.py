5fdbce26548afe47e701a87c44b79e7f4a475a9f
Sofia-Skovoronskaia <79004820+Sofia-Skovoronskaia@users.noreply.github.com>
2021-03-30 13:01:15 +0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render
 from django.views import View
 from django.conf import settings
 
-from django.views.generic.edit import FormView
-from .forms import UserCreateForm
-
-
-# This is a little complex because we need to detect when we are
-# running in various configurations
-
 
 class HomeView(View):
     def get(self, request):
