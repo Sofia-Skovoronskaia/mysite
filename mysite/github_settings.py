cc4addf8ae2a7a07120c9e58abeec2c58a7e0516
Sofia-Skovoronskaia <79004820+Sofia-Skovoronskaia@users.noreply.github.com>
2021-03-30 12:55:07 +0300

    Update github_settings.py

--- mysite/github_settings.py
+++ mysite/github_settings.py
@@ -1,2 +1,10 @@
-SOCIAL_AUTH_GITHUB_KEY = '9b78986d1d450be1603f'
-SOCIAL_AUTH_GITHUB_SECRET = '98fbbb12e6e984070a4dacfea7e04e8354704bc9'
+# For this to work you will need to create your own keys
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App 
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = '9b***86***450***603f'
+SOCIAL_AUTH_GITHUB_SECRET = '98f******6e**407****acfea****e835***4bc9'
